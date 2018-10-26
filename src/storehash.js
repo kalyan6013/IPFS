73ed4717b99e7633c60fc6fb3343bfbf1f1d681a
kalyan6013 <kalyan6013@gmail.com>
2018-10-26 16:27:59 +0530

    First Commit

---
+++ src/storehash.js
@@ -0,0 +1,76 @@
+// import web3 from './web3';
+// //access our local copy to contract deployed on rinkeby testnet
+// //use your own contract address
+// const address = '0x6dACF19Bc27fAbe34D1bc5b1e4CB284F41854F0d';
+// //use the ABI from your contract
+// const abi = [
+// 	{
+// 		"constant": false,
+// 		"inputs": [
+// 			{
+// 				"name": "x",
+// 				"type": "string"
+// 			}
+// 		],
+// 		"name": "sendHash",
+// 		"outputs": [],
+// 		"payable": false,
+// 		"stateMutability": "nonpayable",
+// 		"type": "function"
+// 	},
+// 	{
+// 		"constant": true,
+// 		"inputs": [],
+// 		"name": "getHash",
+// 		"outputs": [
+// 			{
+// 				"name": "x",
+// 				"type": "string"
+// 			}
+// 		],
+// 		"payable": false,
+// 		"stateMutability": "view",
+// 		"type": "function"
+// 	}
+// ]
+// export default new web3.eth.Contract(abi, address);
+
+import web3 from './web3';
+
+//access our local copy to contract deployed on rinkeby testnet
+//use your own contract address
+// const address = '0xb84b12e953f5bcf01b05f926728e855f2d4a67a9';
+const address = '0x902157073a27c181a08a5bc9467f95480ec67905';
+//use the ABI from your contract
+const abi = 
+[
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getHash",
+		"outputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"name": "sendHash",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
+export default new web3.eth.Contract(abi, address);
